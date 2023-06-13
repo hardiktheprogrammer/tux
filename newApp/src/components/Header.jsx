a2db064f226aa78239694842eb020c30329f1f1a
hardikthecoder <85208662+hardiktheprogrammer@users.noreply.github.com>
2023-06-13 22:46:03 +0530

    add temeplete

---
+++ newApp/src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Navbar() {
+  return (
+    <nav>
+      <div>
+        <ul>
+          <li>About </li>
+          <li></li>;<li></li>;
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
